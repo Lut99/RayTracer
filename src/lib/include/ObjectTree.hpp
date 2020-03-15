6c5866abd0a8f3100f609a70cbae7ce22eb7532e
Lut99 <mullertim99@gmail.com>
2020-03-15 17:23:01 +0100

    Begun working on ObjectTree, the hiearchical representation of a list of objects

---
+++ src/lib/include/ObjectTree.hpp
@@ -0,0 +1,26 @@
+/* OBJECT TREE.hpp
+ *   by Lut99
+ *
+ * Created:
+ *   3/15/2020, 5:02:00 PM
+ * Last edited:
+ *   3/15/2020, 5:22:04 PM
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   The ObjectTree class is meant to store a list of objects in an
+ *   efficiently-searchable way. To facilitate moving spheres, the adding
+ *   of the spheres is done in a dumb way, but with optimize() they are
+ *   shuffled in the most optimal way.
+ *   The most optimal way here is defined as //TBD
+**/
+
+#ifndef OBJECTTREE_HPP
+#define OBJECTTREE_HPP
+
+namespace RayTracer {
+    
+}
+
+#endif
